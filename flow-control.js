be339422eaba7d41eb50dce04373e5e85ed83d16
Oscar Munoz <omunozg@emory.edu>
2019-02-15 23:23:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,7 +33,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case 13 <= age && age <= 19 :
+    case 13 :
     return "You are a teenager"
     break;
     default:
